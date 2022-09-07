f02b1de722a71d850bc0bbe741baaecebed6fa21
silver <silver36c@gmail.com>
2022-09-07 14:58:39 +0900

    FIX: baseUrl 수정

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,10 +1,10 @@
 export const BASE_URL = {
-    this: "chatting-frontend-svc.moine.svc.cluster.local",
-    chatting: "chatting-backend-svc.moine.svc.cluster.local",
-    lecture: "lecture-backend-svc.moine.svc.cluster.local",
-    group: "group-backend-svc.moine.svc.cluster.local",
-    auth: "auth-backend-service.moine.svc.cluster.local",
+    react: "http://13.125.32.16:3000",
+    chatting: "http://3.34.172.89:8080",
+    lecture: "http://13.209.29.237:8080",
+    group: "http://43.200.197.190:8080",
+    auth: "http://43.200.156.51:8080",
     // mypage: "http://lecture-backend-svc.moine.svc.cluster.local",
-    recommend: "recommend-backend-svc.moine.svc.cluster.local",
-    react: "moine-frontend-svc.moine.svc.cluster.local"
+    recommend: "http://3.38.49.196:8080",
+    this: "http://3.38.165.113:3001"
   };
